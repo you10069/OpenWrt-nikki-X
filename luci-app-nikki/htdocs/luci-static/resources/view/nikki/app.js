3a0f0a115a14799a46a644e9954d64506a5a2b20
you10069 <104882329+you10069@users.noreply.github.com>
2026-07-18 14:52:22 +0800

    nikki-X

--- luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
+++ luci-app-nikki/htdocs/luci-static/resources/view/nikki/app.js
@@ -201,7 +201,7 @@ return view.extend({
 
         let m, s, o;
 
-        m = new form.Map('nikki', _('Nikki'), `${_('Transparent Proxy with Mihomo on OpenWrt.')} <a href="https://github.com/nikkinikki-org/OpenWrt-nikki/wiki" target="_blank">${_('How To Use')}</a>`);
+        m = new form.Map('nikki', _('Nikki-X'), `${_('Transparent Proxy with Mihomo on OpenWrt.')} <a href="https://github.com/nikkinikki-org/OpenWrt-nikki/wiki" target="_blank">${_('How To Use')}</a>`);
 
         s = m.section(form.TableSection, 'status', _('Status'));
         s.anonymous = true;
